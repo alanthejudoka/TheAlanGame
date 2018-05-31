01677f70fd516b03891feda2f797f5108fe7e4f6
alebod21 <alebod21@bergen.org>
2018-05-31 13:28:06 -0400

    Cole does have rights even though he's 4/5 a person

--- src/Basics_TheCode/Alan_Object.java
+++ src/Basics_TheCode/Alan_Object.java
@@ -1,6 +1,8 @@
 package Basics_TheCode;
 
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class Alan_Object {
     private int x;
@@ -24,9 +26,25 @@ public class Alan_Object {
     public Image getImage() {
         return image;
     }
+    public void loadImage() {
+        ImageIcon x = new ImageIcon("Photos4Game/RealAlan1.jpg");
+        image = x.getImage();
+    }
     public void move(){
         x += dx;
         y += dy;
     }
+
+    public void keyPressed(KeyEvent i){
+        int key = i.getKeyCode();
+
+        if (key == KeyEvent.VK_RIGHT){
+            dx += 4;
+        }
+        if (key == KeyEvent.VK_LEFT){
+            dx -= 4;
+        }
+    }
+
 
 }
