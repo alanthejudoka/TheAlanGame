d3cbbd79196945fc1b53159cc3688c4bee07b0c3
alebod21 <alebod21@bergen.org>
2018-06-01 17:33:27 -0400

    Cole does have rights even though he's 4/5 a person

---
+++ src/Basics_TheCode/MovingSpriteEx.java
@@ -0,0 +1,34 @@
+package Basics_TheCode;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class MovingSpriteEx extends JFrame {
+
+    public MovingSpriteEx() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        add(new Test_Field());
+
+        setTitle("Moving sprite");
+        setSize(400, 300);
+
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+            MovingSpriteEx ex = new MovingSpriteEx();
+            ex.setVisible(true);
+        });
+    }
+
+}
+
