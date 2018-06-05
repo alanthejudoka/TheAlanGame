2db40a40f9b9b2bbceb7cac9e88f319bac2c2ce2
alanthejudoka <alasal21@bergen.org>
2018-06-05 12:55:04 -0400

    Alan is a legend

--- src/Basics_TheCode/Platform.java
+++ src/Basics_TheCode/Platform.java
@@ -2,6 +2,7 @@ package Basics_TheCode;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class Platform extends JFrame{
     private Platform() {
@@ -17,16 +18,30 @@ public class Platform extends JFrame{
     }
 
 }
-class Platforming extends JPanel {
+class Platforming extends Alan_Object  {
+    public static final Color Brown = new Color(139, 69, 19);
+    int x=0, y=0, velX=0, velY=0;
+
+    public Platforming(){
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+    }
+
     @Override
     protected void paintComponent (Graphics g) {
         int w = getWidth();
         int h = getHeight();
 
+        //atmosphere
         g.setColor(Color.ORANGE);
         g.fillRect(0, 0, w, h);
 
-        //g.setColor(Color.GREEN);
-        //g.fillRect(0, );
+        //ground
+        g.setColor(Brown);
+        g.fillRect(0, 800, w, 600);
+
+
+
     }
+
 }
