c035c7f02eb15a1c79eff2adb11dec3b9ae71fa2
alanthejudoka <alasal21@bergen.org>
2018-06-14 12:46:33 -0400

    Alan is a legend

--- src/TheAlanGame_BigScreen/Enemy_ObjectBig.java
+++ src/TheAlanGame_BigScreen/Enemy_ObjectBig.java
@@ -2,16 +2,16 @@ package TheAlanGame_BigScreen;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyEvent;
 
 public class Enemy_ObjectBig extends JFrame{
-    private int x = 40;
+    private int x = 1240;
     private int y = 1600;
     private int dx;
     private int dy;
     private Image image;
     private int w;
     private int h;
+    protected JPanel panel;
 
     public Enemy_ObjectBig(){
         loadImage();
@@ -33,7 +33,7 @@ public class Enemy_ObjectBig extends JFrame{
         }
     }
     public void loadImage() {
-        ImageIcon x = new ImageIcon("Photos4Game/Cole1.png");
+        ImageIcon x = new ImageIcon("Photos4Game/ColeHead1.png");
         image = x.getImage();
     }
     public int getX() {
@@ -45,31 +45,29 @@ public class Enemy_ObjectBig extends JFrame{
     public Image getImage() {
         return image;
     }
-    public void keyPressed(KeyEvent i){
-        int key = i.getKeyCode();
 
-        if (key == KeyEvent.VK_RIGHT){
-            dx = 6;
+    public void movement() {
+        x += dx;
+        y += dy;
+
+        if ((getLeft() < 0) || (getRight() > panel.getWidth())) {
+            dx *= -1;
         }
-        if (key == KeyEvent.VK_LEFT){
-            dx = -6;
-        }
-        if (key == KeyEvent.VK_UP) {
-            dy = -2;
+        if ((getTop() < 0) || (getBottom() > panel.getHeight())) {
+            dy *= -1;
         }
     }
 
-    public void keyReleased(KeyEvent i){
-        int key = i.getKeyCode();
-
-        if (key == KeyEvent.VK_RIGHT){
-            dx = 0;
-        }
-        if (key == KeyEvent.VK_LEFT){
-            dx = 0;
-        }
-        if (key == KeyEvent.VK_UP) {
-            dy = 4;
-        }
+    public int getRight(){
+        return x + dx;
+    }
+    public int getLeft() {
+        return x - dx;
+    }
+    public int getTop() {
+        return y - dy;
+    }
+    public int getBottom() {
+        return y + dy;
     }
 }
